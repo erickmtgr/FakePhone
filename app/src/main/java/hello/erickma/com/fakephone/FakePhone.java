e9223d98c752bc4cb53db9abbbc25c094d8930f5
ericktmtgr <erickma.1234@gmail.com>
2018-02-20 11:39:28 +0100

    First Layout

---
+++ app/src/main/java/hello/erickma/com/fakephone/FakePhone.java
@@ -0,0 +1,13 @@
+package hello.erickma.com.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FakePhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fake_phone);
+    }
+}
